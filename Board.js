df6883c0aef50da342a534476c01024aa9c895fc
Martim Neves <martimfneves@ua.pt>
2020-12-10 16:41:31 +0000

    board.js feito (?)

--- Board.js
+++ Board.js
@@ -18,14 +18,14 @@ class Board{
 		for (var i = 0; i < this.slots.length; i++) {
 			this.slots[i] = new Array(8);
 			for (var j = 0; j < this.slots[i].length; j++) {
-				this.slots[i][j] = new Slot(colorBool,x1,0,z1);
+				this.slots[i][j] = new Slot(colorBool,x1,y,z1,side/8);
 				this.slotDraughtDic[8*i+j] = null;
 				colorBool = !colorBool;
 				z1++;
 			}
 			colorBool = !colorBool;
 			x1++;
-			z = baseVal;
+			z1 = baseVal;
 		}
 		this.selectedSlot = [];
 		this.overSlot = null;
@@ -44,7 +44,7 @@ class Board{
 			var k = blackTeamStartPositions[i][1];
 			var coords = this.slots[j][k].getCoords();
 
-			this.draughts[i] = new Draught(false,coords[0],coords[1],coords[2]);
+			this.draughts[i] = new Draught(false,coords[0],coords[1],coords[2],side/16);
 			this.slotDraughtDic[8*j+k] = this.draughts[i];
 		}
 		for (var i = 0; i < whiteTeamStartPositions.length; i++) {
@@ -52,15 +52,33 @@ class Board{
 			var k = whiteTeamStartPositions[i][1];
 			var coords = this.slots[j][k].getCoords();
 
-			this.draughts[i + blackTeamStartPositions.length] = new Draught(true,coords[0],coords[1],coords[2]);
+			this.draughts[i + blackTeamStartPositions.length] = new Draught(true,coords[0],coords[1],coords[2],side/16);
 			this.slotDraughtDic[8*j+k] = this.draughts[i + blackTeamStartPositions.length];
 		}
 
 		// Captured Draughts
-		//TO DO
-		//
-		//
-		//
+		
+		this.capturedStack = { true : [], false : [] };
+		this.capturedStackBaseLocation = {	true	: [ x-side/2-(2*side/8), y, z-side/2 ],
+											false	: [ x+side/2+(2*side/8), y, z+side/2 ] };
+
+		this.vertices = [x-side/2, y, z-side/2,
+						 x-side/2, y, z+side/2,
+						 x+side/2,y , z-side/2,
+						 x+side/2,y , z+side/2];
+
+		// Computing the triangle normal vector for every vertex
+        this.normals = [];
+        computeVertexNormals( this.vertices, this.normals );
+
+		this.colors = [];
+		this.colors.push( 102 / 255 );
+		this.colors.push( 51 / 255 );
+		this.colors.push( 0 / 255 );
+
+		this.currentTeam = true;
+
+		this.gameOver = false;
 	}
 
 	//Getters
@@ -207,6 +225,314 @@ class Board{
 
 	// Slots Logic
 	
+	moveOverRight() {
+		if(this.overSlot === null) {
+			this.overSlot = [0,7];
+		}
+		else {
+			this.resetOverSlot();
+			this.overSlot[0] = (this.overSlot[0]+1)%8;
+		}
+
+		this.selectOverSlot();
+	}
+
+	moveOverLeft() {
+		if(this.overSlot === null) {
+			this.overSlot = [0,7];
+		}
+		else {
+			this.resetOverSlot();
+			this.overSlot[0] = (this.overSlot[0]+7)%8;
+		}
+
+		this.selectOverSlot();
+	}
+
+	moveOverDown() {
+		if(this.overSlot === null) {
+			this.overSlot = [0,7];
+		}
+		else {
+			this.resetOverSlot();
+			this.overSlot[1] = (this.overSlot[1]+1)%8;
+		}
+
+		this.selectOverSlot();
+	}
+
+	moveOverUp() {
+		if(this.overSlot === null) {
+			this.overSlot = [0,7];
+		}
+		else {
+			this.resetOverSlot();
+			this.overSlot[1] = (this.overSlot[1]+7)%8;
+		}
+
+		this.selectOverSlot();
+	}
+
+	selectOverSlot() {
+		// obtain the slot
+		var xd = this.overSlot[0];
+		var yd = this.overSlot[1];
+		var slot = this.slots[xd][yd];
+
+		// change the color
+		slot.setOverColors();
+		initBuffersSlots();
+	}
+
+	resetOverSlot() {
+		if (this.overSlot[0] != this.selectedSlot[0] ||  this.overSlot[1] != this.selectedSlot[1]) {
+			// obtain the slot
+			var xd = this.overSlot[0];
+			var yd = this.overSlot[1];
+			var slot = this.slots[xd][yd];
+
+			// change the color
+			slot.resetColors();
+			initBuffersSlots();
+		}
+	}
+
+	selectSlot() {
+		if (this.overSlot != null) {
+			if (this.selectedSlot == 0) {
+				// obtain coordinates of the slot
+				var xd = this.overSlot[0];
+				var yd = this.overSlot[1];
+
+				// verify if has a draught and if it's the correct team
+				if (this.slotDraughtDic[8 * xd + yd] === null || this.slotDraughtDic[8 * xd + yd].getTeam() !== this.currentTeam) {
+					return;
+				}
+
+				// Select the slot (selected slot = over slot)
+				this.selectedSlot = [];
+				this.selectedSlot[0] = this.overSlot[0];
+				this.selectedSlot[1] = this.overSlot[1];
+
+				// obtain the slot
+				var slot = this.slots[x][z];
+
+				// change the slot color
+				slot.setSelectedColors();
+				initBuffersSlots();
+			} else {
+				if (this.isValidPlay(this.selectedSlot, this.overSlot, this.currentTeam)) {
+					console.log("Valid play");
+					this.play(this.selectedSlot, this.overSlot, this.currentTeam);
+					this.deselectSlot();
+				} else {
+					console.log("Invalid play");
+				}
+			}
+		}
+	}
+
+	deselectSlot() {
+		// Deselect the slot and change its color
+		// obtain the slot
+		if(this.selectedSlot == []) {
+			return;
+		}
+		var xd = this.selectedSlot[0];
+		var yd = this.selectedSlot[1];
+		var slot = this.slots[xd][yd];
+
+		// change the color
+		slot.resetColors();
+		initBuffersSlots();
+
+		// reset the selected slot
+		this.selectedSlot = [];
+	}
+
+	//Game info
+
+	getGameOver() {
+		return this.gameOver;
+	}
+
+	getWinningTeam() {
+		if (this.capturedStack[true].length == 12) return "Team 1";
+		return "Team 2";
+	}
+
+	getScores() {
+		return [this.capturedStack[true].length, this.capturedStack[false].length]
+	}
+
+	changeTeamPieces(team, color) {	
+		for (var i = 0; i < this.draughts.length; i++) {
+			if (this.draughts[i].getTeam() === team) {
+				// change color
+				this.draughts[i].setColor(color);
+			}
+		}
+
+	}
+
+	// For illumination
+
+	//setMaterial()????????????????????????
+
+	getMaterial() {
+		return [this.kAmbi, this.kDiff, this.kSpec, this.nPhong];
+	}
+}
+
+class Slot {
+
+	// colorBool is boolean for there are two and only two colors
+	// x, y and z are idCoord (center of top face)
+	// s is side
+	// h is height
+	constructor(color,x,y,z,s) {
+		this.colorBool = color;
+		this.idCoords = [x,y,z];
+		this.vertices = [	x-s/2,	y,	z-s/2,
+							x-s/2,	y,	z+s/2,
+							x+s/2,	y,	z-s/2,
+							x+s/2,	y,	z+s/2];
+		this.colors = [];
+		this.resetColors();
+	}
+
+	getCoords() {
+		return this.idCoords;
+	}
+
+	getColorBool() {
+		return this.colorBool;
+	}
+
+	getVertices() {
+		return this.vertices;
+	}
+
+	getColors() {
+		return this.colors;
+	}
+
+	setSelectedColors() {
+		console.log("before" + this.colors);
+		for (var i = 0; i < this.colors.length; i+= 3) {
+			this.colors[i] = 1.0;
+			this.colors[i+1] = 0.27;
+			this.colors[i+2] = 0;
+		}
+		console.log("after" + this.colors);
+	}
+
+	setOverColors() {
+		if(	this.colors[0]!=1 || this.colors[1]!=0.27 || this.colors[2]!=0) {
+			for (var i = 0; i < this.colors.length; i+= 3) {
+				this.colors[i] = 0.48;
+				this.colors[i+1] = 0.98;
+				this.colors[i+2] = 0;
+			}
+		}
+	}
+
+	resetColors() {
+
+		var color = 0.25;
+		if (this.colorBool) {
+			color = 0.75;
+		}
+
+		var length = this.vertices.length;
+		for (var i = 0; i < length; i++) {
+			this.colors[i] = color;
+		}
+	}
+}
+
+class Draught {
+
+	constructor(team, x, y, z, r, h=0.5) {
+		this.height = h;
+		this.radius = r;
+		this.team = team;
+		this.idCoords = [x,y,z];
+		this.colors = [];
+		this.setColor(0);
+		this.diffCoords = [0, 0, 0];
+	}
+
+	getCoords() {
+		return this.idCoords;
+	}
+
+	getTeam() {
+		return this.team;
+	}
+
+	getColors() {
+		return this.colors;
+	}
+
+	getDiffCoords(coords) {
+		return this.diffCoords;
+	}
+
+	getHeight() {
+		return this.height;
+	}
+
+	getRadius() {
+		return this.radius;
+	}
+
+	setCoords(newCoords) {
+		this.idCoords = newCoords;
+	}
+
+	setDiffCoords(coords) {
+		this.diffCoords = coords;
+	}
+
+	//Color
+
+	setColor(colorCode) {
+		if (this.team) {
+			switch (colorCode) {
+				case 1:
+					// Color 0 red
+					this.setColorDraught([255, 0, 0]);
+					break;
+				default:
+					// Color 1 Gray
+					this.setColorDraught([127, 127, 127]);
+					break;
+			}
+		}
+		else {
+			switch (colorCode) {
+				case 1:
+					// Color 0 green
+					this.setColorDraught([0, 255, 0]);
+					break;
+				default:
+					// Color 1 Black
+					this.setColorDraught([0, 0, 0]);
+					break;
+			}
+		}
+
+	}
+
+	setColorDraught(color) {
+		this.colors = [];
+
+		this.colors.push( color[0] / 255);
+		this.colors.push( color[1] / 255);
+		this.colors.push( color[2] / 255);
+
+	}
 }
 
 const materials = {
